5ebbe658bb894060d3e96013d3c1394d929d6d5b
Peter Bona <bonapeti@gmail.com>
2018-09-28 10:11:56 +0200

    Update to Fedora 28

--- install_wifi.sh
+++ install_wifi.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 sudo dnf remove -y broadcom-wl
-sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-27.noarch.rpm https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-27.noarch.rpm
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-28.noarch.rpm https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-28.noarch.rpm
 sudo dnf install -y broadcom-wl kernel-devel
 sudo akmods --force --kernel `uname -r` --akmod wl
 
